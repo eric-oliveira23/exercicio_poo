bf27063047fbafd8c002157d0f03d3f80feb7f37
Eric <ericpoliveira@gmail.com>
2021-12-10 22:11:00 -0300

    exercicio_poo

---
+++ poo_exercicio/Pessoa.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poo_exercicio
+{
+    class Pessoa
+    {
+        public double peso, altura;
+        
+
+        public double imc()
+        {
+            return peso / (altura * altura);
+        }
+
+        public string resulImc(double imc)
+        {
+            string retorno;
+
+            if (imc < 18.5)
+            {
+                retorno = "Abaixo do peso.";
+            }
+            else if (imc < 25)
+            {
+                retorno = "Peso normal";
+            }
+            else if (imc < 30)
+            {
+                retorno = "Acima do peso.";
+            }
+            else if (imc < 35)
+            {
+                retorno = "Obesidade I";
+            }
+            else if (imc < 40)
+            {
+                retorno = "Obesidade II";
+            }
+            else 
+            {
+                retorno = "Obesidade III";
+            }
+
+            return retorno ;
+            
+        }
+
+        //Mensagem
+
+        public void mensagem()
+        {
+            //Obter cálculo
+            double obterCalc = imc();
+
+            //Obter resultado do IMC 
+            string obterIMC = resulImc(obterCalc);
+
+            //Exibir Mensagem
+            
+            Console.WriteLine("Seu IMC é de "+ Math.Round(obterCalc, 2) + ", e a situação do seu IMC é de: " + obterIMC);
+        }
+    }
+}
---
+++ poo_exercicio/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poo_exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+
+            p.peso = 65;
+            p.altura = 1.85;
+            p.mensagem();
+
+            Console.ReadKey();
+        }
+    }
+}
